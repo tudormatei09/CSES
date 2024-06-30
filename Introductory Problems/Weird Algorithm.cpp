7418d449aa5e6dbfbae8cee9478b30d4effc6b28
Holotă Tudor-Matei <125114789+tudormatei09@users.noreply.github.com>
2024-06-30 17:50:56 +0300

    Update Weird Algorithm.cpp

--- Introductory Problems/Weird Algorithm.cpp
+++ Introductory Problems/Weird Algorithm.cpp
@@ -1,1 +1,23 @@
+#include "bits/stdc++.h"
+inline static void solve(){
+        long long int n;
+        std :: cin >> n;
+        std :: cout << n << ' ';
+        while(n != 1){
+            if(n % 2 != 0){
+               n = n * 3 + 1;
+               std :: cout << n << ' ';
+            }else if(n % 2 == 0){
+                n  = n / 2;
+                std :: cout << n << ' ';
+            }
+        }
+}
+signed main(){
+        std :: ios_base :: sync_with_stdio(false);
+        std :: cin.tie(0);
+        std :: cout.tie(0);
+        solve();
+       return 0;
+}
 
